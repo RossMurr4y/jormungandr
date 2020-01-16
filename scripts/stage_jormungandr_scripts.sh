e241ab7f2e903b1d00782237dbfa13bcc4bccc13
Ross Murray <ross.murray@protonmail.com>
2020-01-16 12:09:28 +1100

    builder image doesn't have wget, use curl instead

--- scripts/stage_jormungandr_scripts.sh
+++ scripts/stage_jormungandr_scripts.sh
@@ -5,7 +5,7 @@ git clone "https://github.com/input-output-hk/jormungandr-qa.git"
 cp jormungandr-qa/scripts/* /tmp/bin
 
 # install rossmurr4y node_init script
-wget -P /tmp/bin https://raw.githubusercontent.com/RossMurr4y/daedalus/master/scripts/node_init.sh
+curl https://raw.githubusercontent.com/RossMurr4y/daedalus/master/scripts/node_init.sh -o /tmp/bin/node_init.sh
 
 # Grant execution rights on scripts
 chmod +x /tmp/bin/*.sh
